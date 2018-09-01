6db2699afd087bb53d4961c2a4ef65fa73969f92
Vaastav Arora <arora74@purdue.edu>
2018-09-01 19:53:36 -0400

    game file initial

--- ChessGam/src/ChessGame.java
+++ ChessGam/src/ChessGame.java
@@ -1,3 +1,28 @@
 public class ChessGame {
+    private ChessBoard chessBoard;
+    private Piece[] pieces;
 
+    public ChessGame() {
+        chessBoard = new ChessBoard();
+        pieces = new Piece[16];
+        int a;
+        boolean col;
+        a = 0;
+        col = true;
+        for (int i = 0; i < 2; i++) {
+            pieces[a++] = new Piece("Rook", col);
+            pieces[a++] = new Piece("Knight", col);
+            pieces[a++] = new Piece("Bishop", col);
+            pieces[a++] = new Piece("Queen", col);
+            pieces[a++] = new Piece("King", col);
+            pieces[a++] = new Piece("Bishop", col);
+            pieces[a++] = new Piece("Knight", col);
+            pieces[a++] = new Piece("Rook", col);
+
+            for (int j = 0; j < 8; j++) {
+                pieces[a++] = new Piece("Pawn", col);
+            }
+            col = false;
+        }
+    }
 }
